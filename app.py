8fd8bcb8564dda5b6ef885c7bb414c08c5d765b9
Carlos Antônio da Silva <carlos.antonio@cardeal.corp>
2025-01-30 06:45:21 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/data")
+async def get_data():
+    return {"data": [1, 2, 3]}
